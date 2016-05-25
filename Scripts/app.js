956b6432284de2761954974ed8e664151f9ee66f
ksing419 <300878370@cencol.ca>
2016-05-25 13:19:05 -0400

    try

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+/* main Javascript file */
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+	"use strict";
+	console.log("App started...");
+	
+	var firstParagraph = document.getElementById("firstParagraph");
+	firstParagraph.textContent = "yo";
+}());
